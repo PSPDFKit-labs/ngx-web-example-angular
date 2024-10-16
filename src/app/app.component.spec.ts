1f6aa2c7c0401fe809e611ff7b0abf05d48b0a9b
Hulya Karakaya <hulyakarakayaa06@gmail.com>
2024-10-16 15:52:24 +0500

    Update Angular version

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,9 +4,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      imports: [AppComponent],
     }).compileComponents();
   });
 
@@ -16,7 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ngx-pdf-viewer'`, () => {
+  it(`should have the 'ngx-pdf-viewer' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('ngx-pdf-viewer');
@@ -26,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ngx-pdf-viewer app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ngx-pdf-viewer');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, NgxExtendedPdfViewerModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
